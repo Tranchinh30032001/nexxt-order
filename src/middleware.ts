a972efc7501be3bd05db8499c2267181daff7006
tranchinh <chinhlong30032001@gmail.com>
2024-07-14 23:54:50 +0700

    update-middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,7 +9,6 @@ export function middleware(request: NextRequest) {
   const pathname = request.nextUrl.pathname + request.nextUrl.search
   const accessToken = request.cookies.get('accessToken')?.value
   const refreshToken = request.cookies.get('refreshToken')?.value
-  const isAuth = request.cookies.get('isLogin')?.value
 
   // check pathName is protectedPath
   const isProtectedRoute = protectedPath.some((item) => item.startsWith(request.nextUrl.pathname))
@@ -30,7 +29,7 @@ export function middleware(request: NextRequest) {
    - Đăng nhập rồi và phiên còn có hiệu lực thì không cho vào login/register nữa
    - khi call api bị lỗi 401 khi mà accessToken vẫn hợp lệ => cho ra login luôn
    */
-  if (isAuth && accessToken && (pathname === '/login' || pathname === '/register')) {
+  if (accessToken && (pathname === '/login' || pathname === '/register')) {
     return NextResponse.redirect(new URL('/dashboard', request.url))
   }
 
